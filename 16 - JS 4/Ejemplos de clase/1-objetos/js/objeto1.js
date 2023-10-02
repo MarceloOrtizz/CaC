034e384e71731707d1faaf0bf155d2318061acf6
MarceloOrtiz <marceloortizz@gmail.com>
2023-10-02 18:34:27 -0300

    ejemplo de clase 16

---
+++ 16 - JS 4/Ejemplos de clase/1-objetos/js/objeto1.js
@@ -0,0 +1,12 @@
+// creamos el objeto
+var miAuto = new Object()
+
+// Creamos las propiedades/atributos
+
+miAuto.marca = 'Ford'
+miAuto.modelo = 'Ranger'
+miAuto.anioFabricacion = 2019
+
+// Main: Muesto resultados
+
+console.log("El auto es:", miAuto.marca, miAuto.modelo, "y el año de fabricacion es", miAuto.anioFabricacion)
